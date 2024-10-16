592de8826150f476c4663e1215f9d15818ce2050
Your Namedd <you@example.comd>
2024-10-15 20:01:47 -0500

    Añadiendo elementos al proyecto

---
+++ equipo_futbol/src/equipo_futbol/Equipo_futbol.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package equipo_futbol;
+
+/**
+ *
+ * @author LAB021
+ */
+public class Equipo_futbol {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
